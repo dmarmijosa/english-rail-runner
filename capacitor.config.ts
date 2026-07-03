15a82c3a9e434957a5f9c2a3b77a1997f1ce10fa
Danny Armijos <daniel2667@hotmail.es>
2026-07-03 05:18:54 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'english-rail-runner',
+  webDir: 'www'
+};
+
+export default config;
